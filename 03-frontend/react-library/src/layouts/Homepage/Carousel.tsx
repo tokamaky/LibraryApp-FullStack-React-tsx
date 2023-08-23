53f46e1732e604b98449b9b5b4e1b6a0cd3b0b8a
tokamaky <jxh186045@gmail.com>
2023-08-23 09:20:40 -0400

    update ReturnBook.tsx

--- 03-frontend/react-library/src/layouts/Homepage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/Homepage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook"
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -11,17 +13,9 @@ export const Carousel = () => {
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt='book' />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>JerryLibrary</p>
-                                    <a className="btn main-color text-white" href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                             {/* {books.slice(0, 3).map(book => (
                                 <ReturnBook book={book} key={book.id} />
                             ))} */}
@@ -29,17 +23,9 @@ export const Carousel = () => {
                     </div>
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt='book' />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>JerryLibrary</p>
-                                    <a className="btn main-color text-white" href='#'>Reserve</a>
-                                </div>
-                            </div>
+                             <ReturnBook />
+                             <ReturnBook />
+                             <ReturnBook />
                             {/* {books.slice(3, 6).map(book => (
                                 <ReturnBook book={book} key={book.id} />
                             ))} */}
@@ -48,17 +34,9 @@ export const Carousel = () => {
 
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt='book' />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>JerryLibrary</p>
-                                    <a className="btn main-color text-white" href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                             {/* {books.slice(6, 9).map(book => (
                                 <ReturnBook book={book} key={book.id} />
                             ))} */}
@@ -80,18 +58,7 @@ export const Carousel = () => {
             {/*Mobile */}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-items-center">
-                    <div className="text-center">
-                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                            width='151'
-                            height='233'
-                            alt='book'
-                        />
-                        <h6 className="mt-2">
-                            <b>Book</b>
-                        </h6>
-                        <p>JerryLibrary</p>
-                        <a className="btn main-col text-white" href='#'>Reserve</a>
-                    </div>
+                <ReturnBook />
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
