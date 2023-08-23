15cdc38f965c949407ad6e6b6a532132d8b162d6
Xiaohang Ji <jxh186045@gmail.com>
2023-08-22 22:15:06 -0400

    update Carousel

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
+import { Carousel } from './layouts/Homepage/Carousel';
 
 function App() {
   return (
     <div>     
         <Navbar />
         <ExploreTopBooks />
+        <Carousel />
     </div>
 
   );
--- 03-frontend/react-library/src/layouts/Homepage/Carousel.tsx
+++ 03-frontend/react-library/src/layouts/Homepage/Carousel.tsx
@@ -13,12 +13,13 @@ export const Carousel = () => {
                         <div className='row d-flex justify-content-center align-items-center'>
                             <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
                                 <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png') } 
-                                    width='151'
-                                    height='233'
-                                    alt='book'/>
+                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                        width='151'
+                                        height='233'
+                                        alt='book' />
                                     <h6 className="mt-2">Book</h6>
-                                    <p></p>
+                                    <p>JerryLibrary</p>
+                                    <a className="btn main-color text-white" href='#'>Reserve</a>
                                 </div>
                             </div>
                             {/* {books.slice(0, 3).map(book => (
@@ -28,21 +29,75 @@ export const Carousel = () => {
                     </div>
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
+                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                <div className="text-center">
+                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                        width='151'
+                                        height='233'
+                                        alt='book' />
+                                    <h6 className="mt-2">Book</h6>
+                                    <p>JerryLibrary</p>
+                                    <a className="btn main-color text-white" href='#'>Reserve</a>
+                                </div>
+                            </div>
                             {/* {books.slice(3, 6).map(book => (
                                 <ReturnBook book={book} key={book.id} />
                             ))} */}
                         </div>
                     </div>
+
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
+                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                <div className="text-center">
+                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                                        width='151'
+                                        height='233'
+                                        alt='book' />
+                                    <h6 className="mt-2">Book</h6>
+                                    <p>JerryLibrary</p>
+                                    <a className="btn main-color text-white" href='#'>Reserve</a>
+                                </div>
+                            </div>
                             {/* {books.slice(6, 9).map(book => (
                                 <ReturnBook book={book} key={book.id} />
                             ))} */}
                         </div>
                     </div>
                 </div>
+                <button className="carousel-control-prev" type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className="carousel-control-prev-icon" aria-hidden='true'> </span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type='button'
+                    data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <span className="carousel-control-next-icon" aria-hidden='true'> </span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
 
+            {/*Mobile */}
+            <div className="d-lg-none mt-3">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="text-center">
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
+                            width='151'
+                            height='233'
+                            alt='book'
+                        />
+                        <h6 className="mt-2">
+                            <b>Book</b>
+                        </h6>
+                        <p>JerryLibrary</p>
+                        <a className="btn main-col text-white" href='#'>Reserve</a>
+                    </div>
+                </div>
+            </div>
+            <div className="homepage-carousel-title mt-3">
+                <a className="btn btn-outline-secondary btn-lg" href='#'>View More</a>
             </div>
         </div>
+
     )
 }
