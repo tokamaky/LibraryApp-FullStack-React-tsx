333f93a83069c3aabba97194e358741ba550fc92
Xiaohang Ji <jxh186045@gmail.com>
2023-08-27 20:19:24 -0400

    updaet checkout

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel"
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarsReview";
 
 export const BookCheckoutPage = () =>{
 
@@ -66,9 +67,53 @@ export const BookCheckoutPage = () =>{
         )
     }
     
-    return(
-            <div>
+    return (
+        <div>
+            <div className='container d-none d-lg-block'>
+                <div className='row mt-5'>
+                    <div className='col-sm-2 col-md-2'>
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                                height='349' alt='Book' />
+                        }
+                    </div>
+                    <div className='col-4 col-md-4 container'>
+                        <div className='ml-2'>
+                            <h2>{book?.title}</h2>
+                            <h5 className='text-primary'>{book?.author}</h5>
+                            <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={totalStars} size={32} />
+                        </div>
+                    </div>
+
+                </div>
+                <hr />
+                
+            </div>
+            {/*mobile app */}
+            <div className='container d-lg-none mt-5'>
+                <div className='d-flex justify-content-center alighn-items-center'>
+                    {book?.img ?
+                        <img src={book?.img} width='226' height='349' alt='Book' />
+                        :
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='226'
+                            height='349' alt='Book' />
+                    }
+                </div>
+                <div className='mt-4'>
+                    <div className='ml-2'>
+                        <h2>{book?.title}</h2>
+                        <h5 className='text-primary'>{book?.author}</h5>
+                        <p className='lead'>{book?.description}</p>
+                       
+                    </div>
+                </div>
+
+                <hr />
 
             </div>
-    )
+        </div>
+    );
 }
