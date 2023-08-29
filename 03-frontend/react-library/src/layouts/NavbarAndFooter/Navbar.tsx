c4835fc574e1bf282aa68201688be1cb88ec25c7
tokamaky <jxh186045@gmail.com>
2023-08-29 14:53:37 -0400

    add logout button

--- 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,15 +1,30 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from '@okta/okta-react';
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 // 顶部导航
 export const Navbar = () => {
-    return(
-        <nav className = 'navbar navbar-expand-lg navbar-dark main-color py-3' >
+
+    const { oktaAuth, authState } = useOktaAuth();
+
+    //if it's not in auth
+    if (!authState) {
+        return <SpinnerLoading />
+    }
+
+    const handleLogout = async () => oktaAuth.signOut();
+
+    console.log(authState);
+
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
             <div className='container-fluid'>
                 <span className='navbar-brand'>Luv 2 Read</span>
                 <button className='navbar-toggler' type='button'
                     data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-                    aria-controls='navbarNavDropdown' aira-expanded='false'
-                    aria-label='Toggle Navigation'>
+                    aria-controls='navbarNavDropdown' aria-expanded='false'
+                    aria-label='Toggle Navigation'
+                >
                     <span className='navbar-toggler-icon'></span>
                 </button>
                 <div className='collapse navbar-collapse' id='navbarNavDropdown'>
@@ -18,16 +33,22 @@ export const Navbar = () => {
                             <NavLink className='nav-link' to='/home'>Home</NavLink>
                         </li>
                         <li className='nav-item'>
-                            <NavLink className='nav-link' to='/search'>Search books</NavLink>
+                            <NavLink className='nav-link' to='/search'>Search Books</NavLink>
                         </li>
                     </ul>
                     <ul className='navbar-nav ms-auto'>
-                        <li className='nav-item m-1'>
-                            <a type='button' className='btn btn-outline-light' href='#'>Sign in</a>
-                        </li>
+                        {!authState.isAuthenticated ?
+                            <li className='nav-item m-1'>
+                                <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link>
+                            </li>
+                            :
+                            <li>
+                                <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
+                            </li>
+                        }
                     </ul>
                 </div>
             </div>
-      </nav >
-    )
+        </nav>
+    );
 }
